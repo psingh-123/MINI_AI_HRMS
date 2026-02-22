76a6955e2b3d1e6e35b033a3b53b980affc15fe2
--global <123cs0070@iiitk.ac.in>
2026-02-22 12:42:06 +0530

    Frontend Ready for the deploymnet

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mini-ai-hrms-backend.onrender.com/api",
 });
 
 // Attach token automatically
