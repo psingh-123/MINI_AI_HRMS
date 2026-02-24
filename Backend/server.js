d656316fbfe2eb9bd1a9509330df1dc71b4bc5d3
Prashant Singh <123cs0070@iiitk.ac.in>
2026-02-24 20:35:22 +0530

    Update CORS origin for production environment

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://zenith-hr-twnk.onrender.com",
+      "https://zenith-hr-tpbb.onrender.com",
     ],
     credentials: true,
   })
