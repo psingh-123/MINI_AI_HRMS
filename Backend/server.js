937e2cb9118b37402fa7f4a66ea9a3f5dd4610cc
Prashant Singh <123cs0070@iiitk.ac.in>
2026-02-24 18:57:20 +0530

    Update CORS origin for frontend deployment

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://mini-ai-hrms-frontend.onrender.com",
+      "https://zenith-hj51.onrender.com",
     ],
     credentials: true,
   })
